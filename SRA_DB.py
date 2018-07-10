a902c681d6e646ad6669383c7ad27bc22b557158
GailGolomb1 <39998595+GailGolomb1@users.noreply.github.com>
2018-07-10 14:42:04 -0400

    Add files via upload

---
+++ SRA_DB.py
@@ -0,0 +1,19 @@
+import sqlite3
+conn = sqlite3.connect ('SRAmetadb.sqlite')
+c = conn.cursor()
+
+taxon_id = raw_input("Enter taxon_id: ")
+
+
+library_source = str(raw_input("Enter library_source: "))
+
+
+instrument_model = raw_input("Enter instrument_model: ")
+
+
+number_results = raw_input("Enter number number_results: ")
+
+
+c.execute('SELECT run_accession FROM sra WHERE taxon_id == {0} AND library_source == {1} AND instrument_model == {2} LIMIT {3};'
+.format(taxon_id, library_source, instrument_model, number_results))
+print (c.fetchall())
